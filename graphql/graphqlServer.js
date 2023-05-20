bea6bdca72c535bca06659105ba7afce0668e83b
Laurels <laurels.echichinwo@stu.cu.edu.ng>
2023-05-20 19:13:21 +0100

    cache with 'bounded' option added to apollo server

--- graphql/graphqlServer.js
+++ graphql/graphqlServer.js
@@ -2,7 +2,7 @@ const { ApolloServer } = require('apollo-server-express');
 const typeDefs  = require('./bookSchema');
 const resolvers = require('./bookResolver');
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({  cache: "bounded", persistedQueries: false, typeDefs, resolvers });
 
 const setupGraphQL = async (app) => {
   await server.start();
